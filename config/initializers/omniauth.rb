4438d89152b3c2e1136248d617c0715e8b83f9cc
wizardywizard <littledavid175@gmail.com>
2020-01-06 15:56:57 -0800

    Done.

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,4 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :developer unless Rails.env.production?
+     provider :github, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+  end
